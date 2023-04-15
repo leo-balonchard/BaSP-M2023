812a3688589941b0a8911048ea5a10b8b729bf9e
leo-balonchard <leobalonchard@gmail.com>
2023-04-14 22:33:22 -0300

    BaSP-W05-2023: first block of exercises done

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,40 @@
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+
+/* a. Crear dos variables numéricas y utilizar el operador suma para
+guardar el valor de la suma de ambos números en una 3er variable.*/
+
+console.log('-Exercise 1.a');
+
+var a = 10;
+
+var b = 20;
+
+var addition = a + b;
+
+console.log(addition);
+
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log('-Exercise 1.b');
+
+var string01 = 'Radium ';
+
+var string02 = 'Rocket';
+
+var concatenate = string01 + string02;
+
+console.log(concatenate);
+
+/* c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de varras
+del string) guardando el resultado de la suma en una 3er variable (utilizar length).*/
+
+console.log('-Exercise 1.c');
+
+var myName = 'Leonardo';
+
+var mySurname = 'Balonchard';
+
+var addingCharacters = myName.length + mySurname.length;
+
+console.log(addingCharacters);
+
